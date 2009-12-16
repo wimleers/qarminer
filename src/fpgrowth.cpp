a6b19452ee0c700c709d855f71385625d9a58ea6
Wim Leers <work@wimleers.com>
2009-12-16 19:56:07 +0000

    Transaction parsing now uses Qt's signal/slot mechanism, because C++ callbacks are a nightmare (C callbacks are perfectly doable, C++'s aren't).

--- src/fpgrowth.cpp
+++ src/fpgrowth.cpp
@@ -10,10 +10,18 @@ FPGrowth::FPGrowth(QString filename) {
         qDebug() << this->supportCount[i].first << ":" << this->supportCount[i].second;
 
     qDebug() << "Preprocessing stage 2: parsing support counts";
-    //this->parser.parseTransactions(this->transactionParserCallback);
+    QObject::connect(&(this->parser), SIGNAL(parsedTransaction(Transaction)), this, SLOT(parsedTransaction(Transaction)));
+    this->parser.parseTransactions();
 }
 
 
 void transactionParserCallback(int transaction) {//Transaction transaction) {
+    qDebug() << "parsed" << transaction;
+}
+
+
+//------------------------------------------------------------------------------
+// Private slots.
+void FPGrowth::parsedTransaction(Transaction transaction) {
     qDebug() << "parsed" << transaction;
 }
--- src/fpgrowth.h
+++ src/fpgrowth.h
@@ -1,21 +1,24 @@
 #ifndef FPGROWTH_H
 #define FPGROWTH_H
 
+#include <QObject>
 #include <QHash>
 #include <QString>
 #include "arffparser.h"
 #include "fptree.h"
 #include "fpnode.h"
 
-class FPGrowth {
+class FPGrowth : public QObject {
+    Q_OBJECT
 protected:
     ARFFParser parser;
     QHash<Item, QPair<ItemName, ItemCount> > supportCount;
 
-    void transactionParserCallback(int transaction);
-
 public:
     FPGrowth(QString filename);
+
+private slots:
+    void parsedTransaction(Transaction transaction);
 };
 
 #endif // FPGROWTH_H
